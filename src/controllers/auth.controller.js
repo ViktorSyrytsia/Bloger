695c043fb2560405a74e3949afc44134442ba212
Oleh-Viktor <syrytsiaviktor@gmail.com>
2020-10-19 00:08:21 +0300

    rewrite auth controller and auth logic

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,33 +1,54 @@
+const { NOT_FOUND, UNAUTHORIZED, OK, INTERNAL_SERVER_ERROR, BAD_REQUEST } = require('http-status-codes');
+
 const { UserModel } = require('../models');
+const { fail, success } = require('../helpers/http-response');
+const HttpError = require('../helpers/http-error');
+const userModel = require('../models/user.model');
 
 const register = async (req, res) => {
-
+  try {
+    const user = req.body;
+    if (!user.email) {
+      throw new HttpError(BAD_REQUEST, 'Email is require');
+    }
+    if (!user.password) {
+      throw new HttpError(BAD_REQUEST, 'Password is require');
+    }
+    const userToSave = new userModel(user);
+    userToSave.setPassword(user.password);
+    success(res, OK, {
+      status: 'success',
+      data: (await userToSave.save()).toAuthJson()
+    });
+  } catch (error) {
+    fail(res, new HttpError(error.code || INTERNAL_SERVER_ERROR, error.message));
+  }
 };
 
 const login = async (req, res) => {
-    try {
-        const user = await UserModel.findOne({ email: req.body.email });
-        const isValid = await user.validatePassword(req.body.password);
-        console.log(isValid);
-        if (isValid) {
-            res.status(200).json({
-                status: "ok",
-                token: user.generateJWT()
-            })
-        }
-
-
-    } catch (error) {
-
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
+    if (!user) {
+      throw new HttpError(NOT_FOUND, 'User with this email is not found');
     }
-}
+    const isValid = await user.validatePassword(req.body.password);
+    if (!isValid) {
+      throw new HttpError(UNAUTHORIZED, 'Wrong password');
+    }
+    success(res, OK, await user.generateJWT());
+  } catch (error) {
+    fail(res, new HttpError(error.code || INTERNAL_SERVER_ERROR, error.message));
+  }
+};
 
 const logout = async (req, res) => {
-    res.status(200).json({
-        data: req.user
-    })
+  res.status(200).json({
+    data: req.user
+  });
 };
 
 module.exports = {
-    register, login, logout
-}
+  register,
+  login,
+  logout
+};
--- src/helpers/get-user-from-token.js
+++ src/helpers/get-user-from-token.js
@@ -1,18 +1,25 @@
-const { UserModel } = require('../models/index')
 const jwt = require('jsonwebtoken');
+const { UNAUTHORIZED } = require('http-status-codes');
+
+const { UserModel } = require('../models/index');
+const { fail } = require('./http-response');
+const HttpError = require('./http-error');
 
 exports.me = async (req, res, next) => {
+  try {
     if (req.headers && req.headers.authorization) {
-        const authorization = req.headers.authorization;
-        let decoded
-        try {
-            decoded = jwt.verify(authorization, 'secret');
-        } catch (e) {
-            return res.status(401).send('unauthorized');
-        }
-        const userId = decoded.id;
-        req.user = await UserModel.findById(userId)
-        next()
+      const authorization = req.headers.authorization;
+      const decoded = jwt.verify(authorization, 'secret');
+      if (!decoded) {
+        throw new HttpError(UNAUTHORIZED, 'Unauthorized');
+      }
+      const userId = decoded.id;
+      req.user = await UserModel.findById(userId);
+      next();
+    } else {
+      throw new HttpError(UNAUTHORIZED, 'Authorization headers are required');
     }
-    return res.status(500).json({});
-}
+  } catch (error) {
+    fail(res, error);
+  }
+};
